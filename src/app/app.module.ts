80945aa42b42ffe331878a31b846975cfbcf7466
Don Henton <donhenton@gmail.com>
2018-05-25 11:16:55 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { DropdownDirective } from './components/dropdown/dropdown.directive';
+import { MenuDirective } from './components/dropdown/menu.directive';
+
+
+
+const appRoutes: Routes = [];
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DropdownDirective,
+    MenuDirective,
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
