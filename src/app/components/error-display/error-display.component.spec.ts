39a9b513d1702f526faa4ef1b702835dfbc81643
Don Henton <donhenton@gmail.com>
2018-06-05 15:07:22 -0500

    adding tests 3

--- src/app/components/error-display/error-display.component.spec.ts
+++ src/app/components/error-display/error-display.component.spec.ts
@@ -1,19 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ErrorDisplayComponent } from './error-display.component';
 import { ErrorService } from '../../services/error.service';
+import { ApiError } from '../../services/github.interfaces';
 
 describe('ErrorDisplayComponent', () => {
   let component: ErrorDisplayComponent;
   let fixture: ComponentFixture<ErrorDisplayComponent>;
+  let apiVar;
 
+  let errorData;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErrorDisplayComponent ],
-      providers: [ErrorService]
+      declarations: [ErrorDisplayComponent],
+      providers: [ErrorService
+
+      ]
     })
-    .compileComponents();
-  })) ;
+      .compileComponents();
+
+    apiVar = new ApiError();
+    apiVar.debugMessage = 'fred';
+    apiVar.message = 'fred';
+    apiVar.status = 'ted';
+    apiVar.subErrors = [];
+    apiVar.timestamp = 'bonzo';
+
+    errorData = {
+      url: 'url',
+      type: 'type',
+      headers: {},
+      statusText: 'statusText',
+      ok: 'ok',
+      status: 'status',
+      _body: JSON.stringify({ apierror: apiVar })
+
+
+    };
+
+
+
+
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorDisplayComponent);
@@ -22,8 +50,28 @@ describe('ErrorDisplayComponent', () => {
   });
 
   it('should create', () => {
+    const serv = fixture.debugElement.injector.get(ErrorService);
+    console.log(serv);
     expect(component).toBeTruthy();
   });
+
+  it('should receive an error with existing API error', () => {
+    const serv = fixture.debugElement.injector.get(ErrorService);
+
+
+    serv.processError(errorData);
+    expect(component.error).not.toBeNull();
+
+  });
+  it('should receive an error with null API error', () => {
+    const serv = fixture.debugElement.injector.get(ErrorService);
+
+    errorData['_body'] = JSON.stringify({ apierror: null });
+    serv.processError(errorData);
+    expect(component.error).toBeNull();
+
+  });
+
 });
 
 
